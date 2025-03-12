8872d1d4f713567756b0f088c13957b27f038c29
Kyu <heyimkyu@mailbox.org>
2025-03-12 14:49:02 +0100

    try 2 with gizhubs

--- fabric-cli.nix
+++ fabric-cli.nix
@@ -2,11 +2,18 @@
   meson,
   ninja,
   go,
+  fetchFromGitHub,
   stdenv,
   extraBuildInputs ? []
 }:
   stdenv.mkDerivation {
     name = "fabric-cli";
+    src = fetchFromGitHub {
+      owner = "HeyImKyu";
+      repo = "fabric-cli";
+      rev = "ed7da8aeed726abb9cb0603efa83b693b91d3159";
+      hash = "sha256-NatSzI0vbUxwvrUQnTwKUan0mZYJpH6oyCRaEr0JCB0=";
+    };
     propagatedBuildInputs =
       [
         meson
@@ -23,10 +30,21 @@
       # GDK_PIXBUF_MODULE_FILE="$GDK_PIXBUF_MODULE_FILE" \
       # {python.interpreter} "\$@"
       # EOF
-      echo $out
-      echo $src
-      echo $NIX_BUILD_TOP
-      ${meson}/bin/meson setup --buildtype=release --prefix=/usr $NIX_BUILD_TOP #&& sudo ${meson}/bin/meson install -C $out/build
+      
+
+    # Print debug information
+      echo "Source directory: $src"
+      echo "Build directory: $NIX_BUILD_TOP"
+      echo "out: $out"
+      echo "eodebug ---------------"
+
+      # Run meson setup
+      ${meson}/bin/meson setup $src $out/build --buildtype=release --prefix=$out 
+      ${meson}/bin/meson install -C $out/build
+      # echo $out
+      # echo $src
+      # echo $NIX_BUILD_TOP
+      # ${meson}/bin/meson setup --buildtype=release --prefix=/usr {src} #&& sudo {meson}/bin/meson install -C $out/build
     '';
     meta.mainProgram = "fabric-cli";
   }
