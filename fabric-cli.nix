09176d06b6f4c8dc3e687c5035d29387b0519603
Kyu <heyimkyu@mailbox.org>
2025-03-12 15:27:20 +0100

    build with go build

--- fabric-cli.nix
+++ fabric-cli.nix
@@ -1,55 +1,17 @@
-{
-  meson,
-  ninja,
-  go,
-  fetchFromGitHub,
-  stdenv,
-  extraBuildInputs ? []
+{ buildGoModule
+, fetchFromGitHub
 }:
-  stdenv.mkDerivation {
-    name = "fabric-cli";
-    src = fetchFromGitHub {
-      owner = "HeyImKyu";
-      repo = "fabric-cli";
-      rev = "8872d1d4f713567756b0f088c13957b27f038c29";
-      hash = "sha256-NatSzI0vbUxwvrUQnTwKUan0mZYJpH6oyCRaEr0JCB0=";
-    };
-    propagatedBuildInputs =
-      [
-        meson
-        ninja
-        go
-      ]
-      ++ extraBuildInputs;
-    phases = ["installPhase"];
-    installPhase = ''
-      mkdir -p $out/bin
-      # cat > $out/bin/run-widget << EOF
-      # #!/bin/sh
-      # GI_TYPELIB_PATH=$GI_TYPELIB_PATH \
-      # GDK_PIXBUF_MODULE_FILE="$GDK_PIXBUF_MODULE_FILE" \
-      # {python.interpreter} "\$@"
-      # EOF
-      
 
-    # Print debug information
-      echo "Source directory: $src"
-      echo "Build directory: $NIX_BUILD_TOP"
-      echo "out: $out"
-      echo "eodebug ---------------"
+buildGoModule {
+  pname = "fabric-cli";
+  version = "0.0.2";
 
-      # Run meson setup
-      ${meson}/bin/meson setup $src $out/build --buildtype=release --prefix=$out --sources_dir=$out --output_dir=$out/bin
-      # cd $src
-      echo "cwd: "
-      echo $(pwd)
-      cp $src/* $out -r
-      cd $out
-      ${meson}/bin/meson install -C $out/build
-      # echo $out
-      # echo $src
-      # echo $NIX_BUILD_TOP
-      # {meson}/bin/meson setup --buildtype=release --prefix=/usr {src} #&& sudo {meson}/bin/meson install -C $out/build
-    '';
-    meta.mainProgram = "fabric-cli";
-  }
+  src = fetchFromGitHub {
+    owner = "HeyImKyu";
+    repo = "fabric-cli";
+    rev = "ed7da8aeed726abb9cb0603efa83b693b91d3159";
+    hash = "sha256-NatSzI0vbUxwvrUQnTwKUan0mZYJpH6oyCRaEr0JCB0=";
+  };
+
+  vendorHash = "sha256-3ToIL4MmpMBbN8wTaV3UxMbOAcZY8odqJyWpQ7jkXOc="; # Update this after the first build
+}
